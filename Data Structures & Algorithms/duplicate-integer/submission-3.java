8c0fbe9caa175c23836963dfa1ce6612ef19c28d
Samuel Vieira Fonseca <62188883+samuvf@users.noreply.github.com>
2026-09-16 12:30:48 -0300

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> map = new HashSet<>();
+        for(int n : nums) {
+            if(map.contains(n)) return true;
+            map.add(n);
+        }
+
+        return false;
+    }
+}
